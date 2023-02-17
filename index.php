de07b55245de9c80153e52e04d6914d72143f898
GuglielmoBini <guglielmobini@gmail.com>
2023-02-17 16:49:29 +0100

    created class 'Movie', + costructor, + methods. created 2 objects 'Movie'.

--- index.php
+++ index.php
@@ -7,11 +7,10 @@
 // - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 
 // Bonus 1:
-// Creatre un'istanza Genre e modificare la classe Movie in modo che accetti piú di un genere
+// Creare un'istanza Genre e modificare la classe Movie in modo che accetti piú di un genere
 
 // Bonus 2:
 // Creare un layout completo per stampare a schermo una lista di movies.
-
 // Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
 // - creando un file dedicato ai dati (tipo le array di oggetti) che potremmo chiamare db.php
 // - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models
@@ -19,3 +18,61 @@
 
 
 // ---------------------------------------------------------------------------------
+
+// creo la classe
+class Movie
+{
+    public $title;
+    public $director;
+    public $year;
+    public $plot;
+    public $score;
+
+    // definisco il costruttore
+    public function __construct($_title, $_director, $_year, $_plot, $_score)
+    {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
+        $this->plot = $_plot;
+        $this->score = $_score;
+    }
+
+    // metodi
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getDirector()
+    {
+        return $this->director;
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function getPlot()
+    {
+        return $this->plot;
+    }
+
+    public function getScore()
+    {
+        return $this->score;
+    }
+}
+
+// oggetti
+$babylon = new Movie('Babylon', 'Damien Chazelle', 2022, 'Una storia di ambizioni smisurate ed eccessi oltraggiosi, ripercorre l\'ascesa e la caduta di più personaggi durante un\'era di decadenza e depravazione sfrenata nella prima Hollywood.', 60);
+
+
+$parasite = new Movie('Parasite', 'Bong Joon Ho', 2019, 'L\'intera famiglia di Ki-taek è disoccupata e molto interessata allo stile di vita della ricca famiglia Park finché non sono coinvolti in un incidente inaspettato.', 96);
+
+
+// stampo a schermo
+var_dump($babylon);
+
+var_dump($parasite);
